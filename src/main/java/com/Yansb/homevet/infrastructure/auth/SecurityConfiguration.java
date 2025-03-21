9bd0945f0b60978cfc4b15f6577ab79e7ac3cd16
yansb <yansbarreiro@gmail.com>
2025-03-21 00:03:21 -0300

    fix: cors configuration

--- src/main/java/com/Yansb/homevet/infrastructure/auth/SecurityConfiguration.java
+++ src/main/java/com/Yansb/homevet/infrastructure/auth/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.Yansb.homevet.infrastructure.auth;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,15 +30,28 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorizeRequests -> authorizeRequests
             .requestMatchers(HttpMethod.POST, "/user").permitAll()
             .anyRequest().authenticated())
         .oauth2ResourceServer(oauth -> oauth.jwt(
-          jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-        ))
+            jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
         .build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(
+        Arrays.asList("http://127.0.0.1:5173", "http://localhost:5173", "https://homevet-web.vercel.app/"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   public Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
@@ -43,11 +59,11 @@ public class SecurityConfiguration {
 
     converter.setJwtGrantedAuthoritiesConverter(jwt -> {
       List<String> roles = Optional.ofNullable(jwt.getClaimAsStringList("authorities"))
-        .orElse(List.of());
+          .orElse(List.of());
 
       return roles.stream()
-        .map(SimpleGrantedAuthority::new)
-        .collect(Collectors.toList());
+          .map(SimpleGrantedAuthority::new)
+          .collect(Collectors.toList());
     });
 
     return converter;
